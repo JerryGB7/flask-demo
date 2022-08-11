dd8f687eb111fa2c47ebd6eeaedc6abdd795c181
JerryGB7 <102263967+JerryGB7@users.noreply.github.com>
2022-08-10 21:21:31 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Hello, World! And all who inhabit it!</p>"
 
 @app.route("/<name>")
 def hello_name(name):
