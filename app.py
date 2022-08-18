a4a2b51f26e871c09451da284d94836576bc28e2
JerryGB7 <102263967+JerryGB7@users.noreply.github.com>
2022-08-17 17:13:11 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello world, and all who inhabit it! Look who figured out how to make a jenkins pipeline with docker containers.</p>"
+    return "<p>Hello world, and all who inhabit it! Look who figured out how to make a jenkins pipeline with docker containers.</p><br>That's right bitches it is I."
 
 @app.route("/<name>")
 def hello_name(name):
